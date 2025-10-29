ecdd54646d0a5a91bda78d0ce3a0cc53d7699c09
duartedaniel92 <duartedaniel473@gmail.com>
2025-10-28 21:17:05 -0300

    adicionei uma api para dar os valores atuais das moedas

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.28
-const euro = 6.23
-
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const data = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL')
+        .then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: 'currency',
